660571cdd39fe524f1d8c61c8ad5c9ce62a046b2
Makarand <30976763+makarandmadhavi@users.noreply.github.com>
2020-07-11 12:33:04 +0530

    Update conn.php

--- backend/conn.php
+++ backend/conn.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "Makjets13", "schoolelection");
+    $conn = mysqli_connect("localhost", "root", "", "schoolelection");
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     } 
